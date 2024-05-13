0e99727563b51e94a843add6b9f295b8d932e21d
CoyoProg <corentin.gamedev@gmail.com>
2024-05-13 16:54:34 +0200

    [edit]

--- Plugins/AdvancedQuest/Source/AdvancedQuest/Public/AdvancedQuest.cpp
+++ Plugins/AdvancedQuest/Source/AdvancedQuest/Public/AdvancedQuest.cpp
@@ -1,13 +1,19 @@
-#include "AdvancedQuest.h"
+// Copyright 2024, Coyo Prog, All rights reserved.
 
-static const FName AdvancedQuestTabName("AdvancedQuest");
+#include "AdvancedQuest.h"
 
 #define LOCTEXT_NAMESPACE "FAdvancedQuestModule"
 
+void FAdvancedQuestModule::StartupModule()
+{
+	// This code will execute after your module is loaded into memory; the exact timing is specified in the .uplugin file per-module
+}
 
-
-
-
+void FAdvancedQuestModule::ShutdownModule()
+{
+	// This function may be called during shutdown to clean up your module.  For modules that support dynamic reloading,
+	// we call this function before unloading the module.
+}
 
 #undef LOCTEXT_NAMESPACE
 
--- Plugins/AdvancedQuest/Source/AdvancedQuest/Public/AdvancedQuest.h
+++ Plugins/AdvancedQuest/Source/AdvancedQuest/Public/AdvancedQuest.h
@@ -3,8 +3,13 @@
 #pragma once
 
 #include "CoreMinimal.h"
-#include "Modules/ModuleInterface.h"
+#include "Modules/ModuleManager.h"
 
 class FAdvancedQuestModule : public IModuleInterface
 {
+public:
+
+	/** IModuleInterface implementation */
+	virtual void StartupModule() override;
+	virtual void ShutdownModule() override;
 };
