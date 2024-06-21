f2ff8e19d9295932709b55948a91ea82971f4b4b
CoyoProg <corentin.gamedev@gmail.com>
2024-06-21 17:30:42 +0200

    Fix Project
    Final Fix Project
    
    fix
    
    FIX Project
    
    Update Engine.ini
    
    Fix ?
    
    FIX
    
    FIX PROJECT

---
+++ Source/Quest/QuestGameModeBase.h
@@ -0,0 +1,17 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/GameModeBase.h"
+#include "QuestGameModeBase.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class QUEST_API AQuestGameModeBase : public AGameModeBase
+{
+	GENERATED_BODY()
+	
+};
